470b433875bc4a13bf1a39214e25c698a8cf7f82
Roberto Padilha <robertocesardasilva@gmail.com>
2023-03-06 21:57:39 -0300

    Aula 06/03

--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -1,7 +1,12 @@
 export const formatNumber = (value: number): string => {
+  if (value < 0) {
+    return "-"
+  }
+
   const fmt = new Intl.NumberFormat('pt-BR', {
     minimumFractionDigits: 1, 
-    maximumFractionDigits: 1
+    maximumFractionDigits: 2,
+    minimumIntegerDigits: 1,
   })
 
   return fmt.format(value)
